5caac03c29e781606cb3a88c8303865c8341c2f3
olaborde <oml007@live.com>
2019-02-04 22:50:10 -0500

    created frontend for the dashboard

--- src/main/java/com/example/demo/controllers/RaveController.java
+++ src/main/java/com/example/demo/controllers/RaveController.java
@@ -17,12 +17,37 @@ public class RaveController {
         return "index.html";
     }
 
+    @RequestMapping("/login")
+    public String login(Model model){
+        model.addAttribute("title", "Login");
+        return "login";
+    }
+
+    @RequestMapping("/signup")
+    public String signup(Model model){
+        model.addAttribute("title", "Signup");
+        return "signup";
+    }
+
+    @RequestMapping("/profile")
+    public String profile(Model model){
+        model.addAttribute("title", "Profile");
+        return "profile";
+    }
+
+    @RequestMapping("/dashboard")
+    public String dashboard(Model model){
+        model.addAttribute("title", "Dashboard");
+        return "dashboard";
+    }
+
 
     @RequestMapping("/contact")
     public String contact(Model model){
         model.addAttribute("title", "Contact");
         return "contact_page";
     }
+
     @RequestMapping("/contactSuccess")
     @ResponseBody
     public String contactSuccess(){
